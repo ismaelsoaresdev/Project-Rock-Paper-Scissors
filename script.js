6c97fe74e918fd8a91e721eeda1b6459881bde2c
Ismael <258471924+ismaelsoaresdev@users.noreply.github.com>
2026-03-24 23:09:04 -0300

    Add a UI to Rock Paper Scissors

--- script.js
+++ script.js
@@ -15,22 +15,17 @@ else {return 'scissors'}
 
 
 
-function getHumanChoice() { 
-let choice = prompt('rock,paper or scissors?').toLowerCase()
 
 
-return choice;
-}
 
 
-function playgame(){
 
 
 
 let humanScore = 0
 let computerScore = 0
 
-for (i=0; i<5; i++) { playRound()}
+//for (i=0; i<5; i++) { playRound()}
 
 
 
@@ -38,28 +33,34 @@ function playRound(){
 
 
 
-let humanChoice = getHumanChoice();
+
 let computerChoice = getComputerChoice();
 
 
 
 
-console.log(humanChoice)
-console.log(computerChoice)
+choiceFeedback.textContent = ('you choosed: ' + humanChoice)
+choiceFeedback.textContent += (' | Computer chose: ' + computerChoice)
 
 
 
 
 
-if (humanChoice === computerChoice) {console.log('draw')} else
-if (humanChoice === 'rock' && computerChoice === 'paper') {console.log('You lost! Paper beats Rock'), computerScore++} else
-if (humanChoice === 'rock' && computerChoice === 'scissors') {console.log('You won! Rock beats Scissors'), humanScore++} else
-if (humanChoice === 'scissors' && computerChoice === 'paper') {console.log('You won! Scissors beats Paper'), humanScore++} else
-if (humanChoice === 'scissors' && computerChoice === 'rock') {console.log('You lost! Rock beats Scissors'), computerScore++} else
-if (humanChoice === 'paper' && computerChoice === 'rock') {console.log('You won! Paper beats Rock'), humanScore++} else
-if (humanChoice === 'paper' && computerChoice === 'scissors') {console.log('You lost! Scissors beats Paper'), computerScore++} 
+if (humanChoice === computerChoice) {returnMatchFeedback.textContent = 'draw'} else
+if (humanChoice === 'rock' && computerChoice === 'paper') {returnMatchFeedback.textContent = 'You lost! Paper beats Rock', computerScore++} else
+if (humanChoice === 'rock' && computerChoice === 'scissors') {returnMatchFeedback.textContent = 'You won! Rock beats Scissors', humanScore++} else
+if (humanChoice === 'scissors' && computerChoice === 'paper') {returnMatchFeedback.textContent = 'You won! Scissors beats Paper', humanScore++} else
+if (humanChoice === 'scissors' && computerChoice === 'rock') {returnMatchFeedback.textContent = 'You lost! Rock beats Scissors', computerScore++} else
+if (humanChoice === 'paper' && computerChoice === 'rock') {returnMatchFeedback.textContent = 'You won! Paper beats Rock', humanScore++} else
+if (humanChoice === 'paper' && computerChoice === 'scissors') {returnMatchFeedback.textContent = 'You lost! Scissors beats Paper', computerScore++} 
 
 
+scoreMarker.textContent = `Player: ${humanScore} - Computer: ${computerScore}`;
+
+let = matchEndVerifier = 0
+if (matchEndVerifier & humanScore === 5 || computerScore === 5){ return } else if        
+(humanScore === 5){divannounceWinner.textContent = 'Human Won!!Made 5 points first!!',matchEndVerifier = 1} else if
+(computerScore === 5){divannounceWinner.textContent = 'Computer Won!!Made 5 points first!!',matchEndVerifier= 1}
 
 
 }
@@ -71,5 +72,22 @@ if (humanScore <=  computerScore){console.log('You lost!')}
 
 
 
-}
-playgame()
+
+
+
+
+
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
+rockBtn.addEventListener('click', () => { humanChoice = 'rock'; playRound(); });
+paperBtn.addEventListener('click', () => { humanChoice = 'paper'; playRound(); });          
+scissorsBtn.addEventListener('click', () => { humanChoice = 'scissors'; playRound(); }); 
+
+
+
+const scoreMarker = document.querySelector('#scoreBoard');
+const returnMatchFeedback = document.querySelector('#matchFeedback')
+const choiceFeedback = document.querySelector('#choiceDisplay')
+const divannounceWinner = document.querySelector('#announceWinner')
